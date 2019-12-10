9b7b4db83b6b480536bcc6ba3d4229b1463b9517
sumina <suminamaharjan098@gmail.com>
2019-12-10 16:50:29 +0545

    Coding of Automorphic.

--- app/src/main/java/com/softwarica/secondassignment_fragments/fragments/AutomorphicFragment.java
+++ app/src/main/java/com/softwarica/secondassignment_fragments/fragments/AutomorphicFragment.java
@@ -8,13 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.softwarica.secondassignment_fragments.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AutomorphicFragment extends Fragment {
+public class AutomorphicFragment extends Fragment implements View.OnClickListener{
+
+    private Button btnAutomorphic;
+    private EditText etAutomorphic;
 
 
     public AutomorphicFragment() {
@@ -26,7 +32,38 @@ public class AutomorphicFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_automorphic, container, false);
+        View view = inflater.inflate(R.layout.fragment_automorphic, container, false);
+         etAutomorphic = view.findViewById(R.id.etAutomorphic);
+         btnAutomorphic = view.findViewById(R.id.btnAutomorphic);
+
+         btnAutomorphic.setOnClickListener(this);
+         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        int a,s,c=1;
+        int number = Integer.parseInt(etAutomorphic.getText().toString());
+
+        a=number;
+
+        s=number*number;
+
+        while (number!=0)
+
+        {
+
+            c=c*10;
+
+            number=number/10;
+
+        }
+
+        if(s%c==a)
+            Toast.makeText(getActivity(),"The number is Automorphic.", Toast.LENGTH_SHORT).show();
+
+        else
+            Toast.makeText(getActivity(),"The number is not Automorphic.",Toast.LENGTH_SHORT).show();
+
+    }
 }
