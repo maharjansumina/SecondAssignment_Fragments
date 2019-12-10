2d97f101d866ae4b240b8409f1b170010aa802d8
sumina <suminamaharjan098@gmail.com>
2019-12-10 16:59:13 +0545

    Coding of Palindrome and Swapping.

--- app/src/main/java/com/softwarica/secondassignment_fragments/fragments/PalindromeFragment.java
+++ app/src/main/java/com/softwarica/secondassignment_fragments/fragments/PalindromeFragment.java
@@ -8,13 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.softwarica.secondassignment_fragments.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PalindromeFragment extends Fragment {
+public class PalindromeFragment extends Fragment implements View.OnClickListener{
+
+    private Button btnPalindrome;
+    private EditText etPalindrome;
 
 
     public PalindromeFragment() {
@@ -26,7 +32,32 @@ public class PalindromeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_palindrome, container, false);
+        View view = inflater.inflate(R.layout.fragment_palindrome, container, false);
+
+        etPalindrome = view.findViewById(R.id.etPalindrome);
+        btnPalindrome = view.findViewById(R.id.btnPalindrome);
+
+        btnPalindrome.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        int r,sum=0,temp;
+        int number = Integer.parseInt(etPalindrome.getText().toString());
+
+        temp=number;
+        while(number>0){
+            r=number%10;  //getting remainder
+            sum=(sum*10)+r;
+            number=number/10;
+        }
+        if(temp==sum)
+
+            Toast.makeText(getActivity(),"The number is Palindrome.", Toast.LENGTH_SHORT).show();
+
+        else
+            Toast.makeText(getActivity(),"The number is not Palindrome.",Toast.LENGTH_SHORT).show();
+
+    }
 }
--- app/src/main/java/com/softwarica/secondassignment_fragments/fragments/SwappingFragment.java
+++ app/src/main/java/com/softwarica/secondassignment_fragments/fragments/SwappingFragment.java
@@ -8,13 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.softwarica.secondassignment_fragments.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SwappingFragment extends Fragment {
+public class SwappingFragment extends Fragment implements View.OnClickListener{
+
+    private Button btnSwapping;
+    private EditText etFirst, etSecond;
 
 
     public SwappingFragment() {
@@ -26,7 +32,26 @@ public class SwappingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_swapping, container, false);
+        View view = inflater.inflate(R.layout.fragment_swapping, container, false);
+
+        etFirst = view.findViewById(R.id.etFirst);
+        etSecond = view.findViewById(R.id.etSecond);
+        btnSwapping = view.findViewById(R.id.btnSwapping);
+
+        btnSwapping.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        int first = Integer.parseInt(etFirst.getText().toString());
+        int second = Integer.parseInt(etSecond.getText().toString());
+
+        first = first + second;
+        second = first - second;
+        first = first - second;
+
+        Toast.makeText(getActivity(),"After Swapping" + " first = " + first + ", second = " + second, Toast.LENGTH_SHORT).show();
+
+    }
 }
